0693ca8ed833f22c5a20b91324e4f0256ec498b6
Alice <2211865232@qq.com>
2023-07-04 14:20:03 +0800

    add two new files in notes

---
+++ notes/largerDemo.java
@@ -0,0 +1,18 @@
+public class largerDemo{
+	
+	
+public static void main(String[] args){
+int line=1;
+while(line<=5){
+	int i=1;
+	while(i<=line){
+	System.out.print("*");
+	i=i+1;}
+	System.out.println("");
+	line=line+1;
+}
+
+}
+
+
+}
